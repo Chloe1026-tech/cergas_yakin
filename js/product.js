4fa83047dff92723c8a5833cdc44a7e241a6eee7
Chloe1026-tech <chanchloe1026@gmail.com>
2025-11-03 22:58:26 +0800

    Update product.js

--- js/product.js
+++ js/product.js
@@ -1,74 +1,75 @@
 const products = [
   {
     name: "Dish Wash",
-    image: "images/Dish wash(new label).jpg"
+    image: "Dish wash(new label).jpg"
   },
 
   {
     name: "Floor Cleaner",
-    image: "images/Floor_cleaner.jpg"
+    image: "Floor_cleaner.jpg"
   },
 
   {
     name: "Bleach Water",
-    image: "images/Bleach_water.jpg"
+    image: "Bleach_water.jpg"
   },
 
   {
     name: "Laundry Detergent",
-    image: "images/Laundry.jpg"
+    image: "Laundry.jpg"
   },
 
   {
     name: "Machine Washing",
-    image: "images/Machine_washing.jpg"
+    image: "Machine_washing.jpg"
   },
 
   {
     name: "Multipurpose Cleaner",
-    image: "images/Multipurpose.jpg"
+    image: "Multipurpose.jpg"
   },
 
   {
     name: "Softlan",
-    image: "images/Softlan.jpg"
+    image: "Softlan.jpg"
   },
 
   {
     name: "Toilet Cleaner",
-    image: "images/Toilet_cleaner.jpg"
+    image: "Toilet_cleaner.jpg"
   },
 
 
 
  {
     name: "Glass & Mirror Cleaner",
-    image: "images/mirro cleaner.jpg"
+    image: "mirro cleaner.jpg"
   },
 
   {
     name: "Engine Degreaser",
-    image: "images/Engine Degreaser.jpg"
+    image: "Engine Degreaser.jpg"
   },
 
   {
     name: "Serai Wangi",
-    image: "images/Serai Wangi.jpg"
+    image: "Serai Wangi.jpg"
   },
 
 
   {
     name: "Hand Sanitizer(Gel or Liquid)",
-    image: "images/Hand_sanitizer.jpg"
+    image: "Hand_sanitizer.jpg"
   },
 
   {
     name: "Disinfectant",
-    image: "images/Disinfectant.jpg"
+    image: "Disinfectant.jpg"
   },
 
 
 ];
+
 
 
 
